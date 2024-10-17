bfb94a3695ad07df9035658b04a2df9724912be5
Saket patil <96988509+Saketpatil@users.noreply.github.com>
2024-10-17 18:22:48 +0530

    Add files via upload

---
+++ date.js
@@ -0,0 +1,28 @@
+exports.getDate = () => {
+    const currentDate = new Date();
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+    }
+    return currentDate.toLocaleDateString("en-US", options);
+}
+
+// Another Function
+exports.getDay = function() {
+    const currentDate = new Date();
+    const options = {
+        weekday: "long",
+    }
+    return currentDate.toLocaleDateString("en-US", options);
+}
+
+// Another Function
+exports.getYear = function() {
+    const currentDate = new Date();
+    const options = {
+        month: "long",
+        year: "numeric"
+    }
+    return currentDate.toLocaleDateString("en-US", options);
+}
